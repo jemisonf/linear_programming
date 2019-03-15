f033f0d74eddfc9dbe6610ca19a69da8087f9a68
Sean Gillen <gillens@oregonstate.edu>
2019-03-14 23:45:13 -0700

    working plot with raw data, best curve, linear curve

--- temperature_change/generate_plot.R
+++ temperature_change/generate_plot.R
@@ -1,15 +1,43 @@
-read.csv("Corvallis.csv")
-matplot(data[, 7], data[, 8], type="l")
+data <- read.csv("Corvallis.csv", header=T, sep=";")
 
+x = data[, 9]
+temps = data[, 8] 
 
-#y = function(x){1.7142857*x + 1.8571429}
-#plot(
-#  xs,
-#  ys,
-#  xlim=c(0, 11),
-#  ylim=c(0, 20),
-#  xlab="x",
-#  ylab="y",
-#  main="y = 1.7142857x + 1.8571429 and sample points"
-#)
-#curve(y, add=TRUE, xlim=(c(-2, 20)))
+plot(x,
+     temps,
+     type="l",
+     xlim=c(0, 22450),
+     xlab="Days since May 1, 1952",
+     ylab="Average Temperature (Degrees C)",
+     main="Corvallis Temperature History 1952-2013")
+
+x0 = 8.0214197
+x1 = 0.00010694836
+x2 = 4.2808907
+x3 = 8.1868578
+x4 = -0.79063079
+x5 = -0.29536021
+
+y = function(x){x0 +
+    x1*x+
+    x2*cos(2*pi*x/365.25) + 
+    x3*sin(2*pi*x/365.25) +
+    x4*cos(2*pi*x/365.25/10.7) + 
+    x5*sin(2*pi*x/365.25/10.7)}
+curve(y, 
+      add=TRUE, 
+      xlim=(c(0, 22450)),
+      col='green',
+      xlab="Days since May 1, 1952",
+      ylab="Average Temperature (Degrees C)",
+      lwd=2)
+
+z = function(x){x0 +
+    x1*x}
+curve(z, 
+      add=TRUE, 
+      xlim=(c(0, 22450)),
+      col='red',
+      xlab="Days since May 1, 1952",
+      ylab="Average Temperature (Degrees C)",
+      lwd=5)
