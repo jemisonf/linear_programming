ad29cadced3e708500ec6c00ec45980576b9775d
Sean Gillen <gillens@oregonstate.edu>
2019-03-14 22:10:40 -0700

    add start of r plot script

---
+++ temperature_change/generate_plot.R
@@ -0,0 +1,15 @@
+read.csv("Corvallis.csv")
+matplot(data[, 7], data[, 8], type="l")
+
+
+#y = function(x){1.7142857*x + 1.8571429}
+#plot(
+#  xs,
+#  ys,
+#  xlim=c(0, 11),
+#  ylim=c(0, 20),
+#  xlab="x",
+#  ylab="y",
+#  main="y = 1.7142857x + 1.8571429 and sample points"
+#)
+#curve(y, add=TRUE, xlim=(c(-2, 20)))
